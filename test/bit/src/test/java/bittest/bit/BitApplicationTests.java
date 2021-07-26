966b39848f39dfe12057a67f89659af6cc821c09
yuhwanwoo <6514687@naver.com>
2021-07-27 01:25:55 +0900

    test13

---
+++ test/bit/src/test/java/bittest/bit/BitApplicationTests.java
@@ -0,0 +1,13 @@
+package bittest.bit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
