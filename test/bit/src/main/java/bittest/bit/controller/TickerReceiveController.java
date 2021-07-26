0cccfa0281d73b98652d90fede4d6c35c9bad2a2
yuhwanwoo <6514687@naver.com>
2021-07-27 01:19:25 +0900

    제발

---
+++ test/bit/src/main/java/bittest/bit/controller/TickerReceiveController.java
@@ -0,0 +1,28 @@
+package bittest.bit.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@Controller
+public class TickerReceiveController {
+
+    @Value("${api-key}")
+    private String key;
+
+    @RequestMapping("/get-ticker")
+    @ResponseBody
+    public void receive() throws IOException {
+        URL add = new URL("https://api.bithumb.com/info/balance");
+        HttpURLConnection con = (HttpURLConnection) add.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("test","test");
+    }
+}
