9172c5e8f4465e4dc533360933ccfe82c07d78ce
yuhwanwoo <6514687@naver.com>
2021-07-29 17:01:48 +0900

    api test

--- test/bit/src/main/java/bittest/bit/controller/TickerReceiveController.java
+++ test/bit/src/main/java/bittest/bit/controller/TickerReceiveController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -14,18 +14,36 @@ import java.net.URL;
 @Controller
 public class TickerReceiveController {
 
-    @Value("${api-key}")
-    private String key;
-    @Value("${test1}")
-    private String test;
+    @Value("${conKey}")
+    private String conKey;
+    @Value("${secKey}")
+    private String secKey;
 
     @RequestMapping("/get-ticker")
     @ResponseBody
-    public void receive() throws IOException {
-        URL add = new URL("https://api.bithumb.com/info/balance");
+    public String receive() throws IOException {
+        URL add = new URL("https://api.bithumb.com/public/transaction_history/BTC_KRW");
         HttpURLConnection con = (HttpURLConnection) add.openConnection();
-        System.out.println(key + test);
-//        con.setRequestMethod("POST");
-//        con.setRequestProperty("test","test");
+        con.setRequestMethod("GET");
+//        con.setRequestProperty("count", "1");
+//        con.setRequestProperty("secretKey", secKey);
+        con.setDoOutput(true);
+        OutputStream out = con.getOutputStream();
+        DataOutputStream dataSend = new DataOutputStream(out);
+//        dataSend.writeBytes("&apiKey=" + conKey + "&secretKey=" + secKey);
+        dataSend.writeBytes("&count=1");
+        dataSend.close();
+
+        int res = con.getResponseCode();
+        InputStream receive;
+
+        if(res == 200){
+            receive = con.getInputStream();
+        }else {
+            receive = con.getErrorStream();
+        }
+        InputStreamReader reader = new InputStreamReader(receive);
+        BufferedReader changer = new BufferedReader(reader);
+        return changer.readLine();
     }
 }
