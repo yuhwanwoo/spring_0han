5712128a501f6dda682b6dd017f99e13376dda9b
yuhwanwoo <6514687@naver.com>
2021-07-30 22:37:16 +0900

    api 호출 실패

--- test/bit/src/main/java/bittest/bit/controller/TickerReceiveController.java
+++ test/bit/src/main/java/bittest/bit/controller/TickerReceiveController.java
@@ -3,6 +3,7 @@ package bittest.bit.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -21,11 +22,36 @@ public class TickerReceiveController {
 
     @RequestMapping("/get-ticker")
     @ResponseBody
-    public String receive() throws IOException {
-        URL add = new URL("https://api.bithumb.com/public/transaction_history/BTC_KRW");
+    public String getTicker() throws IOException {
+        URL add = new URL("https://api.bithumb.com/public/ticker/BTC_KRW");
         HttpURLConnection con = (HttpURLConnection) add.openConnection();
         con.setRequestMethod("GET");
-//        con.setRequestProperty("count", "1");
+        con.setDoOutput(true);
+        OutputStream out = con.getOutputStream();
+        DataOutputStream dataSend = new DataOutputStream(out);
+
+        dataSend.close();
+
+        int res = con.getResponseCode();
+        InputStream receive;
+
+        if(res == 200){
+            receive = con.getInputStream();
+        }else {
+            receive = con.getErrorStream();
+        }
+        InputStreamReader reader = new InputStreamReader(receive);
+        BufferedReader changer = new BufferedReader(reader);
+        return changer.readLine();
+    }
+
+    @RequestMapping(value = "/orderbook", method = RequestMethod.GET)
+    @ResponseBody
+    public String receive() throws IOException {
+        URL add = new URL("https://api.bithumb.com/public/orderbook/BTC_KRW");
+        HttpURLConnection con = (HttpURLConnection) add.openConnection();
+        con.setRequestMethod("GET");
+ //       con.setRequestProperty("count", "1");
 //        con.setRequestProperty("secretKey", secKey);
         con.setDoOutput(true);
         OutputStream out = con.getOutputStream();
