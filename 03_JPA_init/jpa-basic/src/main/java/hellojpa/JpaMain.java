376a27ec7e16706ce550ab28ae5d3c76d48bbc56
yuhwanwoo <6514687@naver.com>
2021-08-01 16:39:34 +0900

    영속성 컨텍스트의 1차 캐시

--- 03_JPA_init/jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ 03_JPA_init/jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,7 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,7 +14,19 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class, 1L);
+
+            // 비영속
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJPA");
+
+            //영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
+
+            Member findMember = em.find(Member.class, 101L);
+
             System.out.println("findMember.id = " + findMember.getId());
             System.out.println("findMember.name = " + findMember.getName());
 
