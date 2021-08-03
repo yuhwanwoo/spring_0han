7c7dfb97ea06474ff934b567f8593987a7dc8e5d
yuhwanwoo <6514687@naver.com>
2021-08-04 00:32:23 +0900

    양방향 매핑

--- 03_JPA_init/jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ 03_JPA_init/jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -36,16 +36,19 @@ public class JpaMain {
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeam(team);
             em.persist(member);
 
-            em.flush();
-            em.clear();
+            team.addMember(member);
+            
+//            em.flush();
+//            em.clear();
+            
+            Team findTeam = em.find(Team.class, team.getId());
+            List<Member> members = findTeam.getMembers();
 
-            Member findMember = em.find(Member.class, member.getId());
-
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam = " + findTeam.getName());
+            for (Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
 
             tx.commit();
         } catch (Exception e) {
--- 03_JPA_init/jpa-basic/src/main/java/hellojpa/Member.java
+++ 03_JPA_init/jpa-basic/src/main/java/hellojpa/Member.java
@@ -45,4 +45,5 @@ public class Member {
     public void setTeam(Team team) {
         this.team = team;
     }
+
 }
