d984f9005bd07b02ff1cd8f386fd108d2a856ffd
yuhwanwoo <6514687@naver.com>
2021-08-07 20:00:38 +0900

    many to many

--- 03_JPA_init/jpa-basic/src/main/java/hellojpa/Member.java
+++ 03_JPA_init/jpa-basic/src/main/java/hellojpa/Member.java
@@ -3,7 +3,9 @@ package hellojpa;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -22,6 +24,9 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
