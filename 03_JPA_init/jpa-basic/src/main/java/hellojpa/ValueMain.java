6f60ebe584893a0bf361979aa5f6bbee7a22d18e
yuhwanwoo <6514687@naver.com>
2021-08-09 22:31:21 +0900

    값 타입의 비교

--- 03_JPA_init/jpa-basic/src/main/java/hellojpa/ValueMain.java
+++ 03_JPA_init/jpa-basic/src/main/java/hellojpa/ValueMain.java
@@ -4,12 +4,10 @@ public class ValueMain {
 
     public static void main(String[] args) {
 
-        Integer a = new Integer(10);
-        Integer b = a;
+        Address address1 = new Address("city", "street", "10000");
+        Address address2 = new Address("city", "street", "10000");
 
-        b = 30;
-
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        System.out.println("address1 == address2:" + (address1 == address2));
+        System.out.println("address1 equals address2:" + (address1.equals(address2)));
     }
 }
