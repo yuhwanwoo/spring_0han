7bf09ff8b049e466ab32f7cdd411f34aa57c1886
yuhwanwoo <6514687@naver.com>
2021-08-17 19:56:41 +0900

    비효율적인 경우

--- 03_JPA_init_JPQL/src/main/java/jpql/JpaMain.java
+++ 03_JPA_init_JPQL/src/main/java/jpql/JpaMain.java
@@ -16,25 +16,42 @@ public class JpaMain {
 
         try {
 
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
             Member member1 = new Member();
-            member1.setUsername("관리자1");
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
 
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
 
-            String query = "select function('group_concat', m.username) From Member m";
+            String query = "select m From Member m";
 
-            List<String> result = em.createQuery(query, String.class).getResultList();
+            List<Member> result = em.createQuery(query, Member.class).getResultList();
 
-            for (String s : result) {
-                System.out.println("s = " + s);
+            for (Member member : result) {
+                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+                //회원1, 팀A(SQL)
+                //회원2, 팀A(1차캐시)
+                //회원3, 팀B(SQL)
             }
 
             tx.commit();
