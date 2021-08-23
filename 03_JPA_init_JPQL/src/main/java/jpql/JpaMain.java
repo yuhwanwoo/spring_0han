06e5404eca5e451eac4e5a810a0a951ca1858ff9
yuhwanwoo <6514687@naver.com>
2021-08-23 22:31:38 +0900

    namedQuery

--- 03_JPA_init_JPQL/src/main/java/jpql/JpaMain.java
+++ 03_JPA_init_JPQL/src/main/java/jpql/JpaMain.java
@@ -42,21 +42,12 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-
-            String query = "select t From Team t";
-
-            List<Team> result = em.createQuery(query, Team.class)
-                    .setFirstResult(0)
-                    .setMaxResults(2)
+            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
+                    .setParameter("username", "회원1")
                     .getResultList();
 
-            System.out.println("result = " + result.size());
-
-            for (Team team : result) {
-                System.out.println("team = " + team.getName() + ", members=" + team.getMembers().size());
-                for (Member member : team.getMembers()) {
-                    System.out.println("-> member = " + member);
-                }
+            for (Member member : resultList) {
+                System.out.println("member = " + member);
             }
 
             tx.commit();
