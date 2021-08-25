8cc233a9878eb853b99660e59317d81395c861d0
yuhwanwoo <6514687@naver.com>
2021-08-25 17:35:35 +0900

    view test

--- 04_boot_JPA/jpashop/src/main/java/jpabook/jpashop/HelloController.java
+++ 04_boot_JPA/jpashop/src/main/java/jpabook/jpashop/HelloController.java
@@ -2,12 +2,14 @@ package jpabook.jpashop;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
+    @GetMapping("hello")
     public String hello(Model model) {
-        model.addAllAttributes("data", "hello!!!");
+        model.addAttribute("data", "hello!!!");
 
         return "hello";
     }
