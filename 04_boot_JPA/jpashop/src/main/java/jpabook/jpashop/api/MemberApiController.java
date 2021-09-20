18c34e8ee8968f90ef21a2e661099965ef29efe4
yuhwanwoo <6514687@naver.com>
2021-09-20 22:55:43 +0900

    회원 조회 api

--- 04_boot_JPA/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ 04_boot_JPA/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,14 +24,28 @@ public class MemberApiController {
 
     @GetMapping("/api/v2/members")
     public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
 
+        return new Result(collect.size(), collect);
     }
 
     @Data
     @AllArgsConstructor
     static class Result<T> {
+        private int count;
         private T data;
     }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
+
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
