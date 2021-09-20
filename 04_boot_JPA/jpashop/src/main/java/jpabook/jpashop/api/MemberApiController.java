ce7eda0ff9885265688afdd5ae6ae00aaf39a1d3
yuhwanwoo <6514687@naver.com>
2021-09-20 22:12:05 +0900

    아이패드 이슈..

--- 04_boot_JPA/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ 04_boot_JPA/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,12 +8,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
