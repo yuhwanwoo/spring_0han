dbccfcaec77e87fbb2f04cd82491153d32d6eb32
yuhwanwoo <6514687@naver.com>
2021-12-29 00:39:32 +0900

    연관관계 매핑 remind

--- 03_JPA_init_remind/hellojpa/src/main/java/hellojpa/JpaMain.java
+++ 03_JPA_init_remind/hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -18,7 +18,14 @@ public class JpaMain {
 
         try {
 
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
 
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeamId(team.getId());
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
--- 03_JPA_init_remind/hellojpa/src/main/java/hellojpa/Member.java
+++ 03_JPA_init_remind/hellojpa/src/main/java/hellojpa/Member.java
@@ -6,27 +6,37 @@ import java.util.Date;
 @Entity // Entity는 JPA에서 관리하는 것임을 나타냄
 public class Member {
 
-    @Id
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "USERNAME")
     private String username;
 
-    private Integer age;
+    @Column(name = "TEAM_ID")
+    private Long teamId;
 
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Lob
-    private String description;
-
-    public Member() {
+    public Long getId() {
+        return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
 }
