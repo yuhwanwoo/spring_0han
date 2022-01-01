bc34dcefdf7cf83e0ed0198e3eb09c77735f3050
yuhwanwoo <6514687@naver.com>
2022-01-01 14:38:33 +0900

    연관관계 주의점 remind

--- 03_JPA_init_remind/hellojpa/src/main/java/hellojpa/JpaMain.java
+++ 03_JPA_init_remind/hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -18,21 +18,16 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeam(team);
             em.persist(member);
 
-            Member findMember = em.find(Member.class, member.getId());
-            List<Member> members = findMember.getTeam().getMembers();
+            Team team = new Team();
+            team.setName("TeamA");
+            team.getMembers().add(member);
+            em.persist(team);
 
-            for (Member m : members) {
-                System.out.println("m.getUsername() = " + m.getUsername());
-            }
 
             tx.commit();
         } catch (Exception e) {
