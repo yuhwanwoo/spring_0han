812f534356959128356e7ee9645538d3a56e37e9
yuhwanwoo <6514687@naver.com>
2022-01-14 19:58:04 +0900

    jpa 연관관계 test 시작

---
+++ jpashop_remind/src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Category {
+
+    @Id @GeneratedValue
+    private Long id;
+
+
+}
---
+++ jpashop_remind/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Delivery {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    private DeliveryStatus status;
+}
