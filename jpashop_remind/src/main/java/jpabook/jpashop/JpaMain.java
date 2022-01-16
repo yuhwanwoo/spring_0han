da60fe9f66ab3ff6880809f753eba2063e25c444
yuhwanwoo <6514687@naver.com>
2022-01-16 12:06:29 +0900

    깃 오류 방지 커밋

--- jpashop_remind/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop_remind/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -20,14 +21,12 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Order order = new Order();
-//            order.addOrderItem(new OrderItem());
-            em.persist(order);
 
-            OrderItem orderItem = new OrderItem();
-            orderItem.setOrder(order);
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
 
-            em.persist(orderItem);
+            em.persist(book);
 
             tx.commit();
         } catch (Exception e) {
