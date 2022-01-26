1e577ce98144cda870dba84dc12decdf579733ff
yuhwanwoo <6514687@naver.com>
2022-01-26 22:53:01 +0900

    임베디드 타입 적용

---
+++ 03_JPA_init_remind/hellojpa/src/main/java/hellojpa/Address.java
@@ -0,0 +1,19 @@
+package hellojpa;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+    private String city;
+    private String street;
+    private String zipcode;
+
+    public Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
--- 03_JPA_init_remind/hellojpa/src/main/java/hellojpa/Member.java
+++ 03_JPA_init_remind/hellojpa/src/main/java/hellojpa/Member.java
@@ -1,11 +1,9 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
-public class Member extends BaseEntity {
+public class Member {
 
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
@@ -14,35 +12,11 @@ public class Member extends BaseEntity {
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "TEAM_ID")
-    private Team team;
+    //기간 Period
+    @Embedded
+    private Period workPeriod;
 
-    @ManyToMany
-    @JoinColumn(name = "MEMBER_PRODUCT")
-    private List<Product> products = new ArrayList<>();
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    //주소
+    @Embedded
+    private Address homeAddress;
 }
