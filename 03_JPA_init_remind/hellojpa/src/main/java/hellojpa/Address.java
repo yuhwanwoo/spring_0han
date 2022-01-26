6c1cff936f77c3d31a956c2364de8b7d4016ae5c
yuhwanwoo <6514687@naver.com>
2022-01-26 23:27:23 +0900

    임베디드 타입 내에 equals 구현

--- 03_JPA_init_remind/hellojpa/src/main/java/hellojpa/Address.java
+++ 03_JPA_init_remind/hellojpa/src/main/java/hellojpa/Address.java
@@ -1,6 +1,7 @@
 package hellojpa;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Address {
@@ -15,5 +16,30 @@ public class Address {
         this.city = city;
         this.street = street;
         this.zipcode = zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipcode);
     }
 }
