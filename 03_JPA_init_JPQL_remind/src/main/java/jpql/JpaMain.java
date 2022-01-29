4a1533b9106db415daf486a41d8f3df1efcfa2c8
yuhwanwoo <6514687@naver.com>
2022-01-29 15:13:22 +0900

    JPQL remind init

--- 03_JPA_init_JPQL_remind/src/main/java/jpql/JpaMain.java
+++ 03_JPA_init_JPQL_remind/src/main/java/jpql/JpaMain.java
@@ -1,9 +1,6 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import java.util.List;
 
 public class JpaMain {
@@ -14,10 +11,25 @@ public class JpaMain {
         tx.begin();
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            em.persist(member);
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
 
+            em.flush();
+            em.clear();
+
+            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                                            .setFirstResult(0)
+                                            .setMaxResults(10)
+                                            .getResultList();
+
+            System.out.println("result.size() = " + result.size());
+            for (Member member1 : result) {
+                System.out.println("member1 = " + member1);
+            }
 
             tx.commit();
         } catch (Exception e) {
--- 03_JPA_init_JPQL_remind/src/main/java/jpql/Member.java
+++ 03_JPA_init_JPQL_remind/src/main/java/jpql/Member.java
@@ -45,4 +45,13 @@ public class Member {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
