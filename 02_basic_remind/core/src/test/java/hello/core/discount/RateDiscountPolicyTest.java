dfbc91532870aa488e35681adb5e8f41cc277f96
yuhwanwoo <6514687@naver.com>
2022-02-03 20:43:44 +0900

    cofing 리팩토링 remind

--- 02_basic_remind/core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ 02_basic_remind/core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -2,12 +2,10 @@ package hello.core.discount;
 
 import hello.core.Member.Grade;
 import hello.core.Member.Member;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class RateDiscountPolicyTest {
 
