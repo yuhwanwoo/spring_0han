21a6c7070ce006f213f6455911627b43914282c6
yuhwanwoo <6514687@naver.com>
2022-02-04 18:07:29 +0900

    스프링 싱글톤 유지 remind

---
+++ 02_basic_remind/core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,33 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        //ThreadA: A사용자 10000원 주문
+        int userAPrice = statefulService1.order("userA", 10000);
+        //ThreadB: B사용자 20000원 주문
+        int userBPrice = statefulService2.order("userB", 20000);
+
+        //ThreadA: 사용자A 주문 금액 조회
+        System.out.println("userAPrice = " + userAPrice);
+
+    }
+
+    static class TestConfig {
+
+        @Bean StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
