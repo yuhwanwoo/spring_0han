e53107b4a925d2083c945d885c0896c598365db1
yuhwanwoo <6514687@naver.com>
2022-02-05 17:54:34 +0900

    컴포넌트 스캔 init remind

---
+++ 02_basic_remind/core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,12 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // 예제상 빼기 위해서
+)
+public class AutoAppConfig {
+}
