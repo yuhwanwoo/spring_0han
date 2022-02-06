1b67c29682546d52db4d0e6b0d5bf72f8bc435a4
yuhwanwoo <6514687@naver.com>
2022-02-06 23:43:08 +0900

    조회하는 빈이 모두 필요할 때, map과 list remind

--- 02_basic_remind/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 02_basic_remind/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,17 +1,24 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.discount.DiscountPolicy;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository,@MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
