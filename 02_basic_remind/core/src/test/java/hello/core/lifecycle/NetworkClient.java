1754e2c227039f744958c4c7efa97e22c321adc8
yuhwanwoo <6514687@naver.com>
2022-02-07 22:46:41 +0900

    빈 라이프사이클 끝 remind

--- 02_basic_remind/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ 02_basic_remind/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,10 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {
 
     private String url;
 
@@ -30,15 +33,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    @PostConstruct
+    public void init() {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    @PreDestroy
+    public void close() {
         System.out.println("NetworkClient.destroy");
         disconnect();
     }
