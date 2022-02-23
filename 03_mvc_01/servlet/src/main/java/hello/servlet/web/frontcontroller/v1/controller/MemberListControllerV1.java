6b8b64b9249629dd9cdde7a5671670eaf684775f
yuhwanwoo <6514687@naver.com>
2022-02-23 23:03:23 +0900

    controller List 추가

--- 03_mvc_01/servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
+++ 03_mvc_01/servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
@@ -9,22 +9,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-public class MemberSaveController implements ControllerV1 {
+public class MemberListControllerV1 implements ControllerV1 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
+
     @Override
     public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
-        int age = Integer.parseInt(request.getParameter("age"));
+        List<Member> members = memberRepository.findAll();
 
-        Member member = new Member(username, age);
-        memberRepository.save(member);
+        request.setAttribute("members", members);
 
-        //Model에 데이터를 보관한다.
-        request.setAttribute("member", member);
-
-        String viewPath = "/WEB-INF/views/save-result.jsp";
+        String viewPath = "/WEB-INF/views/members.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
