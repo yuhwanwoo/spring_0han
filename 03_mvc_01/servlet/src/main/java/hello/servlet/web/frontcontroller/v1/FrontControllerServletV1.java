ded374b8da071d2d4a2f9b86f18ecbb5d5729cd7
yuhwanwoo <6514687@naver.com>
2022-02-24 22:49:51 +0900

    FrontController 도입

---
+++ 03_mvc_01/servlet/src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -0,0 +1,21 @@
+package hello.servlet.web.frontcontroller.v1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV1", urlPatterns = "/front-controller/v1/*")
+public class FrontControllerServletV1 extends HttpServlet {
+
+    private Map<String, ControllerV1> controllerMap = new HashMap<>();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
