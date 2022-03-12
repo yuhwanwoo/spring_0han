a9988301ae889a7ed2b69f71b3d6aafba057a147
yuhwanwoo <6514687@naver.com>
2022-03-13 00:59:59 +0900

    정적 리소스와 뷰 템플릿

--- 03_mvc_01/springmvc/src/main/java/hello/springmvc/SpringmvcApplication.java
+++ 03_mvc_01/springmvc/src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -2,7 +2,9 @@ package hello.springmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class SpringmvcApplication {
 
