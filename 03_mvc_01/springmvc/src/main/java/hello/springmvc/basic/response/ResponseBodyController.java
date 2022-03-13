fd5555466a39d784cdaf43aa61ab66ba0178be01
yuhwanwoo <6514687@naver.com>
2022-03-13 20:29:34 +0900

    프로젝트 init

--- 03_mvc_01/springmvc/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ 03_mvc_01/springmvc/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -4,7 +4,6 @@ import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
