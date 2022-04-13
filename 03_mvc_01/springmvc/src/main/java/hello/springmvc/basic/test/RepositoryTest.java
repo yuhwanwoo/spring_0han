429dee0616c5d50b65431a8c6f72446a150f0f54
yuhwanwoo <6514687@naver.com>
2022-04-13 20:38:22 +0900

    native query test

---
+++ 03_mvc_01/springmvc/src/main/java/hello/springmvc/basic/test/RepositoryTest.java
@@ -0,0 +1,16 @@
+package hello.springmvc.basic.test;
+
+public class RepositoryTest {
+
+//    public static void main(String[] args) {
+//        String sql = "select m.username as username from member m";
+//        List<MemberDto> result = em.createNativeQuery(sql)
+//                .setFirstResult(0)
+//                .setMaxResults(10)
+//                .unwrap(NativeQuery.class)
+//                .addScalar("username")
+//                .setResultTransformer(Transformers.aliasToBean(MemberDto.class))
+//                .getResultList();
+//
+//    }
+}
--- 03_mvc_01/springmvc/src/test/java/hello/springmvc/basic/test/TestAppTest.java
+++ 03_mvc_01/springmvc/src/test/java/hello/springmvc/basic/test/TestAppTest.java
@@ -30,4 +30,22 @@ class TestAppTest {
         }
     }
 
+//    @Test
+//    public void projections() throws Exception {
+//        //given
+//        Team teamA = new Team("teamA");
+//        em.persist(teamA);
+//        Member m1 = new Member("m1", 0, teamA);
+//        Member m2 = new Member("m2", 0, teamA);
+//        em.persist(m1);
+//        em.persist(m2);
+//        em.flush();
+//        em.clear();
+//        //when
+//        List<UsernameOnly> result =
+//                memberRepository.findProjectionsByUsername("m1");
+//        //then
+//        Assertions.assertThat(result.size()).isEqualTo(1);
+//    }
+
 }
