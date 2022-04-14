58183c104609fcb92bd20371b2218d071b963a47
yuhwanwoo <6514687@naver.com>
2022-04-14 21:27:26 +0900

    네이티브 쿼리 및 데이터 jpa 끝

--- 05_data_JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ 05_data_JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -69,4 +69,13 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
     List<Member> findLockByUsername(String name);
 
     <T> List<T> findProjectionsByUsername(@Param("username") String username, Class<T> type);
+
+    @Query(value = "select * from member where username = ?", nativeQuery = true)
+    Member findByNativeQuery(String username);
+
+    @Query(value = "SELECT m.member_id as id, m.username, t.name as teamName " +
+            "FROM member m left join team t",
+            countQuery = "SELECT count(*) from member",
+            nativeQuery = true)
+    Page<MemberProjection> findByNativeProjection(Pageable pageable);
 }
