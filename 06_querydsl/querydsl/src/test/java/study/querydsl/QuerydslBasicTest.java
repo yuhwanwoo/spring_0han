af27548f67eeac9267d2b6b50025196c8a0067aa
yuhwanwoo <6514687@naver.com>
2022-04-25 23:45:00 +0900

    join on절 01

--- 06_querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ 06_querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -255,4 +255,21 @@ public class QuerydslBasicTest {
                 .extracting("username")
                 .containsExactly("teamA", "teamB");
     }
+
+    /**
+     * 예) 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     * JPQL: select m, t from Member m left join m.team t on t.name = 'teamA'
+     */
+    @Test
+    public void join_on_filtering() {
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team).on(team.name.eq("teamA"))
+                .fetch();
+        
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
 }
