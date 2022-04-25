e9cc36d4f0072035b76a51a3db8685699b005a76
yuhwanwoo <6514687@naver.com>
2022-04-26 00:01:58 +0900

    join on절 02

--- 06_querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ 06_querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -267,7 +267,28 @@ public class QuerydslBasicTest {
                 .from(member)
                 .leftJoin(member.team, team).on(team.name.eq("teamA"))
                 .fetch();
-        
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     * 연관 관계 없는 엔티티 외부 조인
+     * 회원의 이름이 팀 이름과 같은 대상 외부 조인
+     * */
+    @Test
+    public void join_on_no_relation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name))
+                .fetch();
+
         for (Tuple tuple : result) {
             System.out.println("tuple = " + tuple);
         }
