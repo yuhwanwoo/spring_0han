86108de9cdc189e28447a4819003456a9969c9ca
yuhwanwoo <6514687@naver.com>
2022-04-30 16:27:41 +0900

    restdocs test;

--- 06_querydsl/querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ 06_querydsl/querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -5,6 +5,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
@@ -12,6 +15,8 @@ import study.querydsl.entity.QHello;
 import javax.persistence.EntityManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @Transactional
@@ -34,6 +39,29 @@ class QuerydslApplicationTests {
 
 		assertThat(result).isEqualTo(hello);
 		assertThat(result.getId()).isEqualTo(hello.getId());
+	}
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void commons() throws Exception {
+
+		this.mockMvc.perform(
+						get("/docs")  // (1)
+								.accept(MediaType.APPLICATION_JSON)
+				);
+
+				// restDoc 테스트
+//				.andExpect(status().isOk())
+//				.andDo(
+//
+//						document("common",
+//						customResponseFields("custom-response", beneathPath("data.genders").withSubsectionId("genders"), // (2)
+//								attributes(key("title").value("성별")),
+//								enumConvertFieldDescriptor(Gender.values())
+//						)
+//				));
 	}
 
 }
