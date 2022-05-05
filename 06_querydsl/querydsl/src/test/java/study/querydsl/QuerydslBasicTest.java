3e64198d11e1b08d6c36baaa894c4cd458c7758b
yuhwanwoo <6514687@naver.com>
2022-05-05 17:39:34 +0900

    projection 적용

--- 06_querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ 06_querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.QMemberDto;
 import study.querydsl.dto.UserDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
@@ -588,6 +589,18 @@ public class QuerydslBasicTest {
                 .fetch();
 
         for (UserDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+    }
+
+    @Test
+    public void findDtoByQueryProjection() {
+        List<MemberDto> result = queryFactory
+                .select(new QMemberDto(member.username, member.age))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
             System.out.println("memberDto = " + memberDto);
         }
     }
