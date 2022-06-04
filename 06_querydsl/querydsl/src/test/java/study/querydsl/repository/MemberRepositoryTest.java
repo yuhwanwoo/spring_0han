d25a07808db93f88b346f477d76be9896c914a30
yuhwanwoo <6514687@naver.com>
2022-06-04 20:24:09 +0900

    스프링 데이터 JPA와 같이 사용

--- 06_querydsl/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ 06_querydsl/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 
@@ -23,6 +26,7 @@ class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
+
     @Test
     public void basicTest() {
         Member member = new Member("member1", 10);
@@ -36,6 +40,35 @@ class MemberRepositoryTest {
 
         List<Member> result2 = memberRepository.findByUsername("member1");
         assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    public void searchTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> result = memberRepository.search(condition);
+
+        for (MemberTeamDto memberTeamDto : result) {
+            System.out.println("memberTeamDto.getUsername() = " + memberTeamDto.getUsername());
+        }
+
+        assertThat(result).extracting("username").containsExactly("member4");
     }
 
 }
