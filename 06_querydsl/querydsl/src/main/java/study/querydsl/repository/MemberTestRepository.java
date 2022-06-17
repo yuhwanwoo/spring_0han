557eb78f462a8082f7ed5662d2cb33076ce2d0a2
yuhwanwoo <6514687@naver.com>
2022-06-17 21:08:35 +0900

    지원클래스 직접 만들기

--- 06_querydsl/querydsl/src/main/java/study/querydsl/repository/MemberTestRepository.java
+++ 06_querydsl/querydsl/src/main/java/study/querydsl/repository/MemberTestRepository.java
@@ -1,7 +1,28 @@
 package study.querydsl.repository;
 
 import org.springframework.stereotype.Repository;
+import study.querydsl.entity.Member;
+import study.querydsl.repository.support.Querydsl4RepositorySupport;
+
+import java.util.List;
+
+import static study.querydsl.entity.QMember.member;
 
 @Repository
-public class MemberTestRepository {
+public class MemberTestRepository extends Querydsl4RepositorySupport {
+
+    public MemberTestRepository(Class<?> domainClass) {
+        super(Member.class);
+    }
+
+    public List<Member> basicSelect() {
+        return select(member)
+                .from(member)
+                .fetch();
+    }
+
+    public List<Member> basicSelectFrom() {
+        return selectFrom(member)
+                .fetch();
+    }
 }
