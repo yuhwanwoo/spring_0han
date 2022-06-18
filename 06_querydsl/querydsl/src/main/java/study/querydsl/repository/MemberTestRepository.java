ef91fc3e77da92487db72738bcb62cbd8365a0c9
yuhwanwoo <6514687@naver.com>
2022-06-18 18:33:54 +0900

    기본적 pagination

--- 06_querydsl/querydsl/src/main/java/study/querydsl/repository/MemberTestRepository.java
+++ 06_querydsl/querydsl/src/main/java/study/querydsl/repository/MemberTestRepository.java
@@ -1,12 +1,20 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
+import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.entity.Member;
 import study.querydsl.repository.support.Querydsl4RepositorySupport;
 
 import java.util.List;
 
+import static org.springframework.util.StringUtils.hasText;
 import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
 
 @Repository
 public class MemberTestRepository extends Querydsl4RepositorySupport {
@@ -25,4 +33,31 @@ public class MemberTestRepository extends Querydsl4RepositorySupport {
         return selectFrom(member)
                 .fetch();
     }
+
+    public Page<Member> searchPageByApplyPage(MemberSearchCondition condition, Pageable pageable) {
+        JPAQuery<Member> query = selectFrom(member)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                );
+        List<Member> content = getQuerydsl().applyPagination(pageable, query)
+                .fetch();
+        return PageableExecutionUtils.getPage(content, pageable, query::fetchCount);
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        return hasText(username) ? member.username.eq(username) : null;
+    }
+    private BooleanExpression teamNameEq(String teamName) {
+        return hasText(teamName) ? team.name.eq(teamName): null;
+    }
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe != null ? member.age.goe(ageGoe) : null;
+    }
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe != null ? member.age.loe(ageLoe) : null;
+    }
+
 }
