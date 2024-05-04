238f201517cab2193955703a0bc23457a1525c42
yuhwanwoo <6514687@naver.com>
2024-05-04 14:05:23 +0900

    메시지 적용

--- message/src/test/java/hello/itemservice/domain/message/MessageSourceTest.java
+++ message/src/test/java/hello/itemservice/domain/message/MessageSourceTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+
+import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 public class MessageSourceTest {
@@ -16,7 +20,35 @@ public class MessageSourceTest {
     @Test
     void helloMessage() {
         String result = messageSource.getMessage("hello", null, null);
-        System.out.println("result = " + result);
         assertThat(result).isEqualTo("안녕");
+    }
+
+    @Test
+    void notFoundMessageCode() {
+        assertThatThrownBy(() -> messageSource.getMessage("no_code", null, null))
+                .isInstanceOf(NoSuchMessageException.class);
+    }
+
+    @Test
+    void notFoundMessageCodeDefaultMessage() {
+        String result = messageSource.getMessage("not_code", null, "기본 메시지", null);
+        assertThat(result).isEqualTo("기본 메시지");
+    }
+
+    @Test
+    void argumentMessage() {
+        String message = messageSource.getMessage("hello.name", new Object[]{"Spring"}, null);
+        assertThat(message).isEqualTo("안녕 Spring");
+    }
+
+    @Test
+    void defaultLang() {
+        assertThat(messageSource.getMessage("hello", null, null)).isEqualTo("안녕");
+        assertThat(messageSource.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
+    }
+
+    @Test
+    void enLang() {
+        assertThat(messageSource.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
     }
 }
