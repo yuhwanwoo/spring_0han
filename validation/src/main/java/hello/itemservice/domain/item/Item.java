69837e1ac82142fe8bf6ebf7048daa137163c831
yuhwanwoo <6514687@naver.com>
2024-05-29 19:00:43 +0900

    bean validation을 통한 처리

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,27 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+/*
+`@NotBlank` : 빈값 + 공백만 있는 경우를 허용하지 않는다.
+`@NotNull` : `null` 을 허용하지 않는다.
+`@Range(min = 1000, max = 1000000)` : 범위 안의 값이어야 한다. `@Max(9999)` : 최대 9999까지만 허용한다.
+ */
 @Data
 public class Item {
-
     private Long id;
+    @NotBlank
     private String itemName;
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
