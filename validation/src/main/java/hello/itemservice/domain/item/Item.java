4608983f721a53ae1777403ec4e753404d589162
yuhwanwoo <6514687@naver.com>
2024-06-05 09:22:55 +0900

    Valid 메시지 커스터마이징 및 자동화 예시

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,6 +2,7 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
@@ -13,6 +14,7 @@ import javax.validation.constraints.NotNull;
 `@Range(min = 1000, max = 1000000)` : 범위 안의 값이어야 한다. `@Max(9999)` : 최대 9999까지만 허용한다.
  */
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 넘게 입력해주세요.")
 public class Item {
     private Long id;
     @NotBlank
