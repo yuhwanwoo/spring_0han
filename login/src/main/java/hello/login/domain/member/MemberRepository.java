6b4164f3fc4ecbe75337f524118c1ada61469c21
yuhwanwoo <6514687@naver.com>
2024-06-15 13:34:50 +0900

    초기 클래스 생성

---
+++ login/src/main/java/hello/login/domain/member/MemberRepository.java
@@ -0,0 +1,29 @@
+package hello.login.domain.member;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+public class MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        log.info("save: member={}", member);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
