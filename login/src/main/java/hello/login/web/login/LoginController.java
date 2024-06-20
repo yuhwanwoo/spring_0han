8349eb46143bd9eed50e56f9910018e215122aea
yuhwanwoo <6514687@naver.com>
2024-06-21 07:03:36 +0900

    로그인 폼 생성

---
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -0,0 +1,21 @@
+package hello.login.web.login;
+
+import hello.login.domain.login.LoginService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final LoginService loginService;
+
+    @GetMapping("/login")
+    public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
+        return "login/loginForm";
+    }
+}
