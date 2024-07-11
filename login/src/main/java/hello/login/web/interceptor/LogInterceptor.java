5de9cfc8078957c0770e536e0d5baa250491d5a0
yuhwanwoo <6514687@naver.com>
2024-07-12 07:09:12 +0900

    인터페이스를 통한 처리

--- login/src/main/java/hello/login/web/interceptor/LogInterceptor.java
+++ login/src/main/java/hello/login/web/interceptor/LogInterceptor.java
@@ -12,14 +12,18 @@ import java.util.UUID;
 @Slf4j
 public class LogInterceptor implements HandlerInterceptor {
     public static final String LOG_ID = "logId";
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
         String uuid = UUID.randomUUID().toString();
         request.setAttribute(LOG_ID, uuid);
-//@RequestMapping: HandlerMethod
-//정적 리소스: ResourceHttpRequestHandler if (handler instanceof HandlerMethod) {
-        HandlerMethod hm = (HandlerMethod) handler; //호출할 컨트롤러 메서드의 모든 정보가 포함되어 있다.
+
+        //@RequestMapping: HandlerMethod
+        // 정적 리소스: ResourceHttpRequestHandler
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod hm = (HandlerMethod) handler; //호출할 컨트롤러 메서드의 모든 정보가 포함되어 있다.
+        }
 
         log.info("REQUEST  [{}][{}][{}]", uuid, requestURI, handler);
         return true; //false 진행X
@@ -27,11 +31,16 @@ public class LogInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+        log.info("postHandle [{}]", modelAndView);
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+        String requestURI = request.getRequestURI();
+        String logId = (String)request.getAttribute(LOG_ID);
+        log.info("RESPONSE [{}][{}]", logId, requestURI);
+        if (ex != null) {
+            log.error("afterCompletion error!!", ex);
+        }
     }
 }
