399c71538a52c2e927d985a35e0634b25fad8887
yuhwanwoo <6514687@naver.com>
2024-07-21 09:50:04 +0900

    인터셉터 예외처리 init

--- hello/src/main/java/com/exception/hello/WebConfig.java
+++ hello/src/main/java/com/exception/hello/WebConfig.java
@@ -1,15 +1,25 @@
 package com.exception.hello;
 
 import com.exception.hello.filter.LogFilter;
+import com.exception.hello.intercepter.LogInterceptor;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error", "/error-page/**");
+    }
+
     @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean();
