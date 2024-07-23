e32c4e964a05cd572aebdcafe7bad88573204174
yuhwanwoo <6514687@naver.com>
2024-07-24 07:24:51 +0900

    api 예외처리 init

--- hello/src/main/java/com/exception/hello/WebServerCustomizer.java
+++ hello/src/main/java/com/exception/hello/WebServerCustomizer.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerF
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
     @Override
     public void customize(ConfigurableServletWebServerFactory factory) {
