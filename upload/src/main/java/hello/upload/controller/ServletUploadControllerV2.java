61666cffc7444a9e744e811d79c5f7abbb5b440d
yuhwanwoo <6514687@naver.com>
2024-08-16 23:31:36 +0900

    기본 제공 업로드 후 저장

---
+++ upload/src/main/java/hello/upload/controller/ServletUploadControllerV2.java
@@ -0,0 +1,63 @@
+package hello.upload.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.Part;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+
+@Slf4j
+@Controller
+@RequestMapping("/servlet/v2")
+public class ServletUploadControllerV2 {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    @GetMapping("/uplaod")
+    public String newFile() {
+        return "upload-form";
+    }
+
+    @PostMapping("/upload")
+    public String saveFileV1(HttpServletRequest request) throws ServletException, IOException {
+        log.info("request={}", request);
+
+        String itemName = request.getParameter("itemName");
+        log.info("itemName={}", itemName);
+
+        Collection<Part> parts = request.getParts();
+        log.info("parts={}", parts);
+
+        for (Part part : parts) {
+            log.info("==== part ====");
+            log.info("name={}", part.getName());
+            Collection<String> headerNames = part.getHeaderNames();
+            for (String headerName : headerNames) {
+                log.info("header {}: {}", headerName, part.getHeader(headerName));
+            }
+            //편의 메서드
+            //content-disposition; filename
+            log.info("submittedFilename={}", part.getSubmittedFileName());
+            log.info("size={}", part.getSize()); // part body size
+
+            //데이터 읽기
+            InputStream inputStream = part.getInputStream();
+            String body = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+            log.info("body={}", body);
+
+        }
+
+        return "upload-form";
+    }
+}
