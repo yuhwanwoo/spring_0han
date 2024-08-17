cca62b5ea548e61205d070c050cf1f94e618209b
yuhwanwoo <6514687@naver.com>
2024-08-17 14:14:29 +0900

    스프링이 제공하는 업로드

--- upload/src/main/java/hello/upload/controller/ServletUploadControllerV2.java
+++ upload/src/main/java/hello/upload/controller/ServletUploadControllerV2.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,6 +57,12 @@ public class ServletUploadControllerV2 {
             String body = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
             log.info("body={}", body);
 
+            //파일에 저장하기
+            if (StringUtils.hasText(part.getSubmittedFileName())) {
+                String fullPath = fileDir + part.getSubmittedFileName();
+                log.info("파일 저장 fullPath={}", fullPath);
+                part.write(fullPath);
+            }
         }
 
         return "upload-form";
