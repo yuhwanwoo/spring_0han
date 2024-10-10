698d903193156da3dcc33481558e3c56aee67a19
yuhwanwoo <6514687@naver.com>
2024-10-11 07:09:51 +0900

    mybatis 설정 추가

--- itemservice-db/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
+++ itemservice-db/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -1,4 +1,4 @@
-package hello.itemservice.repository.jdbctemplate.mybatis;
+package hello.itemservice.repository.mybatis;
 
 import hello.itemservice.domain.Item;
 import hello.itemservice.repository.ItemSearchCond;
