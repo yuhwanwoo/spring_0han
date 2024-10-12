eb544073433d23bcf4a047e37a7ec4a1d8a7a28a
yuhwanwoo <6514687@naver.com>
2024-10-12 22:36:58 +0900

    프록시 적용 확인

--- itemservice-db/src/main/java/hello/itemservice/repository/mybatis/MyBatisItemRepository.java
+++ itemservice-db/src/main/java/hello/itemservice/repository/mybatis/MyBatisItemRepository.java
@@ -6,15 +6,18 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class MyBatisItemRepository implements ItemRepository {
     private final ItemMapper itemMapper;
     @Override
     public Item save(Item item) {
+        log.info("itemMapper class={}", itemMapper.getClass());
         itemMapper.save(item);
         return item;
     }
