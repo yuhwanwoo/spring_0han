1e7c728f56c97b63c6e5b321f358f3f9dc6c1eef
yuhwanwoo <6514687@naver.com>
2024-11-12 09:19:55 +0900

    비즈니스 예외 추가

--- springtx/src/test/java/hello/springtx/order/OrderServiceTest.java
+++ springtx/src/test/java/hello/springtx/order/OrderServiceTest.java
@@ -34,4 +34,18 @@ class OrderServiceTest {
 //then: 롤백되었으므로 데이터가 없어야 한다.
         Optional<Order> orderOptional = orderRepository.findById(order.getId()); assertThat(orderOptional.isEmpty()).isTrue();
     }
+
+    @Test
+    void bizException() {
+//given
+        Order order = new Order(); order.setUsername("잔고부족");
+//when
+        try {
+            orderService.order(order); fail("잔고 부족 예외가 발생해야 합니다.");
+        } catch (NotEnoughMoneyException e) {
+            log.info("고객에게 잔고 부족을 알리고 별도의 계좌로 입금하도록 안내");
+        }
+//then
+        Order findOrder = orderRepository.findById(order.getId()).get();
+        assertThat(findOrder.getPayStatus()).isEqualTo("대기"); }
 }
