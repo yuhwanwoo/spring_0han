53ba7cae9a678838cc8877807064d416ded219dc
yuhwanwoo <6514687@naver.com>
2024-12-13 18:10:35 +0900

    스레드 로컬 테스트

--- advanced/src/test/java/hello/advanced/trace/threadlocal/code/ThreadLocalService.java
+++ advanced/src/test/java/hello/advanced/trace/threadlocal/code/ThreadLocalService.java
@@ -4,7 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 public class ThreadLocalService {
     private ThreadLocal<String> nameStore = new ThreadLocal<>();
     public String logic(String name) {
-        log.info("저장 name={} -> nameStore={}", name, nameStore.get()); nameStore.set(name);
+        log.info("저장 name={} -> nameStore={}", name, nameStore.get());
+        nameStore.set(name);
         sleep(1000);
         log.info("조회 nameStore={}",nameStore.get());
         return nameStore.get();
