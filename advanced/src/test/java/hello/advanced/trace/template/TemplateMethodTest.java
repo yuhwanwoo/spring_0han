72ed9b91672dbe1757f21eaf2baeacfc64fb93a2
yuhwanwoo <6514687@naver.com>
2024-12-24 20:28:38 +0900

    익명 내부 클래스 사용

--- advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -35,12 +35,36 @@ public class TemplateMethodTest {
     }
 
     /**
-     * 템플릿 메서드 패턴 적용 */
+     * 템플릿 메서드 패턴 적용
+     */
     @Test
     void templateMethodV1() {
         AbstractTemplate template1 = new SubClassLogic1();
         template1.execute();
         AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+    }
+
+    /**
+     * 템플릿 메서드 패턴, 익명 내부 클래스 사용
+     */
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름1={}", template1.getClass());
+        template1.execute();
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름2={}", template2.getClass());
         template2.execute();
     }
 }
