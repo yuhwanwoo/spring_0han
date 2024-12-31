764b94345cb0dde8a45c42d11a27c461baa45b00
yuhwanwoo <6514687@naver.com>
2024-12-31 23:34:39 +0900

    전략 패턴 exam2

--- advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -34,13 +34,39 @@ public class ContextV1Test {
     }
 
     /**
-     * 전략 패턴 적용 */
+     * 전략 패턴 적용
+     */
     @Test
     void strategyV1() {
         Strategy strategyLogic1 = new StrategyLogic1();
         ContextV1 context1 = new ContextV1(strategyLogic1);
         context1.execute();
         Strategy strategyLogic2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
+    }
+
+    /**
+     * 전략 패턴 익명 내부 클래스1
+     */
+    @Test
+    void strategyV2() {
+        Strategy strategyLogic1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("strategyLogic1={}", strategyLogic1.getClass());
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+        Strategy strategyLogic2 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        log.info("strategyLogic2={}", strategyLogic2.getClass());
         ContextV1 context2 = new ContextV1(strategyLogic2);
         context2.execute();
     }
