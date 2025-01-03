e646aaf11e9676044ab371831d3e22ebf31c5bb1
yuhwanwoo <6514687@naver.com>
2025-01-03 22:35:31 +0900

    주입을 이용한 적용

---
+++ advanced/src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,20 @@
+package hello.advanced.trace.strategy;
+import hello.advanced.trace.strategy.code.strategy.ContextV2;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+@Slf4j
+public class ContextV2Test {
+    /**
+     * 전략 패턴 적용 */
+    @Test
+    void strategyV1() {
+        ContextV2 context = new ContextV2();
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
+    }
+
+
+}
