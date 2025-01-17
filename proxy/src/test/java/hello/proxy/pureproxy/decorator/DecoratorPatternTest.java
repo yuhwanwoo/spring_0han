9a97ac841d2502bb5706b9e501ab6b04401ce796
yuhwanwoo <6514687@naver.com>
2025-01-17 21:20:41 +0900

    데코레이터 패턴 확인

--- proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -12,4 +12,12 @@ public class DecoratorPatternTest {
         DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
         client.execute();
     }
+
+    @Test
+    void decorator1() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+        client.execute();
+    }
 }
