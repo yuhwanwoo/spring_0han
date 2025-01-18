4c90164f7402de22c3855f7ae6a3badb1bcae096
yuhwanwoo <6514687@naver.com>
2025-01-18 09:39:27 +0900

    패턴적용 모듈 추가

--- proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -20,4 +20,13 @@ public class DecoratorPatternTest {
         DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
         client.execute();
     }
+
+    @Test
+    void decorator2() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+        client.execute();
+    }
 }
