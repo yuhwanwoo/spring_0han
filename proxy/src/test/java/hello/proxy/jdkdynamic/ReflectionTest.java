3cd32d240d19457eceb45abf3888f96846c5155d
yuhwanwoo <6514687@naver.com>
2025-01-26 08:57:39 +0900

    리플렉션 예시2

--- proxy/src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
+++ proxy/src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
@@ -50,4 +50,21 @@ public class ReflectionTest {
         Object result2 = methodCallB.invoke(target);
         log.info("result2={}", result2);
     }
+
+    @Test
+    void reflection2() throws Exception {
+        Class classHello =
+                Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
+        Hello target = new Hello();
+        Method methodCallA = classHello.getMethod("callA");
+        dynamicCall(methodCallA, target);
+        Method methodCallB = classHello.getMethod("callB");
+        dynamicCall(methodCallB, target);
+    }
+
+    private void dynamicCall(Method method, Object target) throws Exception {
+        log.info("start");
+        Object result = method.invoke(target);
+        log.info("result={}", result);
+    }
 }
