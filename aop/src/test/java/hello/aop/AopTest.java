48593245dc921b8ae49a6217a632a1f8c1162649
yuhwanwoo <6514687@naver.com>
2025-02-23 14:36:19 +0900

    aop 구현 1

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -2,17 +2,21 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 @SpringBootTest
+//@Import(AspectV1.class)
+@Import(AspectV2.class)
 public class AopTest {
     @Autowired
     OrderService orderService;
